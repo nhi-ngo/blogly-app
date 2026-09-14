94e889e30c81313aa5b62682440599eaf8bbec4d
nhi-ngo <nhingo.y@gmail.com>
2026-09-14 07:15:14 -0500

    set up project

---
+++ src/main/java/com/nhi/blogly/BloglyApplication.java
@@ -0,0 +1,13 @@
+package com.nhi.blogly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloglyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloglyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhi/blogly/BloglyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhi.blogly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloglyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
