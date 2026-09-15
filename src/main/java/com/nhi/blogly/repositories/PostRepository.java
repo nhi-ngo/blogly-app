82ad773c139a15fd66e4130bb0ccb040e70e4ed0
nhi-ngo <nhingo.y@gmail.com>
2026-09-14 20:15:57 -0500

    feat(repository): create post, tag, user repos

---
+++ src/main/java/com/nhi/blogly/repositories/PostRepository.java
@@ -0,0 +1,12 @@
+package com.nhi.blogly.repositories;
+
+import com.nhi.blogly.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+}
---
+++ src/main/java/com/nhi/blogly/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nhi.blogly.repositories;
+
+import com.nhi.blogly.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
