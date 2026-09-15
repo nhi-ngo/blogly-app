e3bea0e6b516961c3ef021b61ce2066c19b86a81
nhi-ngo <nhingo.y@gmail.com>
2026-09-15 14:16:58 -0500

    feat(auth service): implement auth service

---
+++ src/main/java/com/nhi/blogly/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,61 @@
+package com.nhi.blogly.services.impl;
+
+import com.nhi.blogly.repositories.UserRepository;
+import com.nhi.blogly.services.AuthenticationService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiryMs;
+
+    @Override
+    public UserDetails authenticate(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        return userDetailsService.loadUserByUsername(email);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>(); //claim is information stored inside the JWT
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact(); //converts the JWT builder into the actual JWT string
+    }
+
+    //converts secret string into a cryptographic Key
+    private Key getSigninKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
