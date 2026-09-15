fa524977cbbe658216cdf621db49fc0de67c6292
nhi-ngo <nhingo.y@gmail.com>
2026-09-15 10:13:28 -0500

    feat(auth): create dtos and auth service

---
+++ src/main/java/com/nhi/blogly/domain/dtos/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.nhi.blogly.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
