3740ccf14ab87f5159b27ed42a5a3e0a88ec4509
nhi-ngo <nhingo.y@gmail.com>
2026-09-15 16:24:22 -0500

    feat(auth): extract username and validate token method

--- src/main/java/com/nhi/blogly/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/nhi/blogly/services/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nhi.blogly.services.impl;
 
 import com.nhi.blogly.services.AuthenticationService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,8 +53,24 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .compact(); //converts the JWT builder into the actual JWT string
     }
 
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private String extractUsername(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+
     //converts secret string into a cryptographic Key
-    private Key getSigninKey() {
+    private SecretKey getSigninKey() {
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
