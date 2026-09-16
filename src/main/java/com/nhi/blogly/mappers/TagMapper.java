542d55730b1ccec5cfccb428e7b71b446f30823f
nhi-ngo <nhingo.y@gmail.com>
2026-09-16 13:52:30 -0500

    feat(tags): create tag response dto and tag mapper

---
+++ src/main/java/com/nhi/blogly/mappers/TagMapper.java
@@ -0,0 +1,29 @@
+package com.nhi.blogly.mappers;
+
+import com.nhi.blogly.domain.PostStatus;
+import com.nhi.blogly.domain.dtos.TagResponse;
+import com.nhi.blogly.domain.entities.Post;
+import com.nhi.blogly.domain.entities.Tag;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TagMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    TagResponse toTagResponse(Tag tag);
+
+    @Named("calculatePostCount")
+    default Integer calculatePostCount(List<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return (int) posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
