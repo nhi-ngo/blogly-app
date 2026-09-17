6f1969467d11184312ca5bf93e963022cb4873b6
nhi-ngo <nhingo.y@gmail.com>
2026-09-17 12:04:06 -0500

    feat(tags): create getTagByIds method

--- src/main/java/com/nhi/blogly/services/TagService.java
+++ src/main/java/com/nhi/blogly/services/TagService.java
@@ -15,4 +15,6 @@ public interface TagService {
     void deleteTag(UUID id);
 
     Tag getTagById(UUID id);
+
+    List<Tag> getTagByIds(Set<UUID> ids);
 }
--- src/main/java/com/nhi/blogly/services/impl/TagServiceImpl.java
+++ src/main/java/com/nhi/blogly/services/impl/TagServiceImpl.java
@@ -65,4 +65,13 @@ public class TagServiceImpl implements TagService {
         return tagRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Tag not found with id: " + id));
     }
+
+    @Override
+    public List<Tag> getTagByIds(Set<UUID> ids) {
+        List<Tag> foundTags = tagRepository.findAllById(ids);
+        if (foundTags.size() != ids.size()) {
+            throw new EntityNotFoundException("Not all specified tag IDs exist");
+        }
+        return foundTags;
+    }
 }
