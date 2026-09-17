c1f95e5cd890e3d30b993b870b1d9ef39f47ecd7
nhi-ngo <nhingo.y@gmail.com>
2026-09-17 06:36:51 -0500

    feat(exception): handle EntityNotFoundException error

--- src/main/java/com/nhi/blogly/controllers/ErrorController.java
+++ src/main/java/com/nhi/blogly/controllers/ErrorController.java
@@ -1,6 +1,7 @@
 package com.nhi.blogly.controllers;
 
 import com.nhi.blogly.domain.dtos.ApiErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -64,5 +65,15 @@ public class ErrorController {
                 .build();
 
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
