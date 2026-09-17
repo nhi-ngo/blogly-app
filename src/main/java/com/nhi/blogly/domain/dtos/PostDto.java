bc9a901c2e62a2d64fe59f3fc46213c60cc10b07
nhi-ngo <nhingo.y@gmail.com>
2026-09-17 06:34:24 -0500

    feat(posts): create Post dto and Author dto

---
+++ src/main/java/com/nhi/blogly/domain/dtos/PostDto.java
@@ -0,0 +1,38 @@
+package com.nhi.blogly.domain.dtos;
+
+import com.nhi.blogly.domain.PostStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostDto {
+
+    private UUID id;
+
+    private String title;
+
+    private String content;
+
+    private AuthorDto author;
+
+    private CategoryDto category;
+
+    private Set<TagDto> tags;
+
+    private Integer readingTime;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private PostStatus postStatus;
+}
