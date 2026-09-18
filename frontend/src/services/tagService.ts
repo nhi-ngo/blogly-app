2aee30a35acd9faaf73651274c036da010bf796a
nhi-ngo <nhingo.y@gmail.com>
2026-09-18 09:45:44 -0500

    feat: build tags CRUD endpoints

---
+++ frontend/src/services/tagService.ts
@@ -0,0 +1,22 @@
+import { apiService } from './apiService';
+import { Tag } from '../types/Tag';
+
+class TagService {
+  private api = apiService.getApi();
+
+  public async getTags(): Promise<Tag[]> {
+    const response = await this.api.get<Tag[]>('/tags');
+    return response.data;
+  }
+
+  public async createTag(names: string[]): Promise<Tag[]> {
+    const response = await this.api.post<Tag[]>('/tags', { names });
+    return response.data;
+  }
+
+  public async deleteTag(id: string): Promise<void> {
+    await this.api.delete(`/tags/${id}`);
+  }
+}
+
+export const tagService = new TagService();
